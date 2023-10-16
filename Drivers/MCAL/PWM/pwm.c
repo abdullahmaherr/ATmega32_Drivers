58bd8041985f977005838116fedf7ce19aaccfa8
Abdullah Maher <abdullahhegazi10@gmail.com>
2023-10-16 15:10:29 +0300

    Update

--- Drivers/MCAL/PWM/pwm.c
+++ Drivers/MCAL/PWM/pwm.c
@@ -24,7 +24,7 @@
 /*===============================================================================
  *                              API Definitions                                  *
  ================================================================================*/
-void MCAL_TIMER0_PWMInit(void)
+void MCAL_TIMER0_PWMInit(uint8_t a_prescaler)
 {
 	/* Configure The OC0 Pin as Output */
 	SET_BIT(DDRB,3);
@@ -32,12 +32,43 @@ void MCAL_TIMER0_PWMInit(void)
 	/* Set The Timer Initial Value to 0 */
 	TCNT0 = 0;
 
-	/* Configure the FAST PWM,Non inverting Mode, /8 Prescsaler */
-	TCCR0 = (0x6A);
+	/* Configure the FAST PWM,Non inverting Mode, Prescsaler */
+	TCCR0 = (0x68) | (a_prescaler);
 }
 
 
-void MCAL_TIMER2_PWMInit(void)
+void MCAL_TIMER1_PWMInit(uint8_t a_prescaler,uint8_t a_top, uint8_t a_pin)
+{
+	/* Set The Timer Initial Value to 0 */
+	TCNT1 = 0;
+
+	/*Set TOP Count For Timer1 in ICR1 Register*/
+	ICR1 = a_top;
+
+	/* Configure The OC1A/OC1B Pins as Output */
+	switch(a_pin)
+	{
+	case PWM_OC1A:
+		SET_BIT(DDRD,5);
+		break;
+
+	case PWM_OC1B:
+		SET_BIT(DDRD,4);
+		break;
+
+	case PWM_OC1AB:
+		SET_BIT(DDRD,4);
+		SET_BIT(DDRD,5);
+		break;
+	}
+
+	/* Configure the FAST PWM,Non inverting Mode, Prescsaler */
+	TCCR1A = (0x02) | (a_pin);
+	TCCR1B = (0x18) | (a_prescaler);
+}
+
+
+void MCAL_TIMER2_PWMInit(uint8_t a_prescaler)
 {
 	/* Configure The OC2 Pin as Output */
 	SET_BIT(DDRD,7);
@@ -46,13 +77,29 @@ void MCAL_TIMER2_PWMInit(void)
 	TCNT2 = 0;
 
 	/* Configure the FAST PWM,Non inverting Mode, /8 Prescsaler */
-	TCCR2 = (0x6A);
+	TCCR2 = (0x68) | (a_prescaler);
 }
 
 
 void MCAL_TIMER0_PWMSetDuty(uint8_t a_duty)
 {
 	OCR0 = a_duty;
+}
+
+
+void MCAL_TIMER1_PWMSetDuty(uint8_t a_duty, uint8_t a_pin)
+{
+	switch(a_pin)
+	{
+	case PWM_OC1A:
+		OCR1A = a_duty; break;
+	case PWM_OC1B:
+		OCR1B = a_duty; break;
+	case PWM_OC1AB:
+		OCR1B = a_duty;
+		OCR1A = a_duty;
+		break;
+	}
 }
 
 
--- Drivers/MCAL/PWM/pwm.h
+++ Drivers/MCAL/PWM/pwm.h
@@ -27,31 +27,45 @@
 /*===============================================================================
  *                       Macros Configuration References                         *
  ================================================================================*/
+/* @ref Prescaler */
+#define PWM_No_CLK  					(0U)
+#define PWM_CLK_PRE_1  					(1U)
+#define PWM_CLK_PRE_8  					(2U)
+#define PWM_CLK_PRE_64  				(3U)
+#define PWM_CLK_PRE_256 				(4U)
+#define PWM_CLK_PRE_1024 				(5U)
+#define PWM_CLK_EXT_FAILING  			(6U)
+#define PWM_CLK_EXT_RAISING  			(7U)
 
+ /* Pin Output Selection for Timer1 */
+#define PWM_OC1A 						(0x80U)
+#define PWM_OC1B 						(0x20U)
+#define PWM_OC1AB 						(0xA0U)
 
 /**===============================================================================
  * Function Name  : MCAL_TIMER0_PWMInit.
  * Brief          : Function To Initialize The Fast PWM Mode In Timer0.
- * Parameter (in) : None.
+ * Parameter (in) :	Prescaler Regarding to @ref Prescaler.
  * Return         : None.
- * Note           : Timer0 will be used in FAST PWM with pre-scaler F_CPU/8, Non inverting, F_PWM=(F_CPU)/(256*N) = (10^6)/(256*8) = 500Hz. */
-void MCAL_TIMER0_PWMInit(void);
+ * Note           : Timer0 will be used in FAST PWM, Non inverting, F_PWM = (F_CPU)/(256*N) . */
+void MCAL_TIMER0_PWMInit(uint8_t a_prescaler);
 
 /**===============================================================================
  * Function Name  : MCAL_TIMER1_PWMInit.
  * Brief          : Function To Initialize The Fast PWM Mode In Timer1.
- * Parameter (in) : None.
+ * Parameter (in) :  Prescaler Regarding to @ref Prescaler.
+ * Parameter (in) :  OC1A or OC1B.
  * Return         : None.
- * Note           : None.
-void MCAL_TIMER1_PWMInit(void);*/
+ * Note           : Timer2 will be used in FAST PWM, Non inverting, F_PWM = (F_CPU)/(TOP*N) . */
+void MCAL_TIMER1_PWMInit(uint8_t a_prescaler,uint8_t a_top, uint8_t a_pin);
 
 /**===============================================================================
  * Function Name  : MCAL_TIMER2_PWMInit.
  * Brief          : Function To Initialize The Fast PWM Mode In Timer2.
- * Parameter (in) : None.
+ * Parameter (in) : Prescaler Regarding to @ref Prescaler.
  * Return         : None.
- * Note           : Timer2 will be used in FAST PWM with pre-scaler F_CPU/8, Non inverting, F_PWM=(F_CPU)/(256*N) = (10^6)/(256*8) = 500Hz.*/
-void MCAL_TIMER2_PWMInit(void);
+ * Note           : Timer2 will be used in FAST PWM, Non inverting, F_PWM = (F_CPU)/(256*N) . */
+void MCAL_TIMER2_PWMInit(uint8_t a_prescaler);
 
 /**===============================================================================
  * Function Name  : MCAL_TIMER0_PWMInit.
@@ -66,8 +80,8 @@ void MCAL_TIMER0_PWMSetDuty(uint8_t a_duty);
  * Brief          : Function To Initialize The Required Duty Cycle.
  * Parameter (in) : The Required Duty Cycle.
  * Return         : None.
- * Note           : None.
-void MCAL_TIMER1_PWMSetDuty(uint8_t a_duty);*/
+ * Note           : None.																		*/
+void MCAL_TIMER1_PWMSetDuty(uint8_t a_duty, uint8_t a_pin);
 
 /**===============================================================================
  * Function Name  : MCAL_TIMER2_PWMInit.
