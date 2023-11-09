1c020cfa85b82c58b1f6159b8645a3d0669040c0
Abdullah Maher <abdullahhegazi10@gmail.com>
2023-11-09 11:22:30 +0200

    Update

--- Drivers/MCAL/SPI/spi.c
+++ Drivers/MCAL/SPI/spi.c
@@ -15,51 +15,85 @@
  ================================================================================*/
 #include "spi.h"
 #include "gpio.h"
+#include <avr/interrupt.h>
+
+
+/*===============================================================================
+ *                              Global Variables                                 *
+ ================================================================================*/
+
+static volatile void (*gp_CallBack)(void) = NULL_PTR;  /* Global Pointer To Assign ISRs */
+
+
+/*===============================================================================
+ *                       		 ISR Functions  		                         *
+ ================================================================================*/
+ISR(SPI_STC_vect)
+{
+	/* Calling Handler of OVF Mode */
+	if(gp_CallBack != NULL_PTR)
+		(*gp_CallBack)();
+}
+
 
 /*===============================================================================
  *                              API Definitions                                  *
  ================================================================================*/
 
-void MCAL_SPI_InitMaster(void)
+void MCAL_SPI_init(SPI_Config_t *p_SPI_Config)
 {
 	/*Configure The Pins of SPI*/
-	MCAL_GPIO_PinInit(GPIOB_ID, GPIO_PIN4, GPIO_PIN_OUTPUT);/*SS*/
-	MCAL_GPIO_PinInit(GPIOB_ID, GPIO_PIN5, GPIO_PIN_OUTPUT);/*MOSI*/
-	MCAL_GPIO_PinInit(GPIOB_ID, GPIO_PIN6, GPIO_PIN_INPUT);/*MISO*/
-	MCAL_GPIO_PinInit(GPIOB_ID, GPIO_PIN7, GPIO_PIN_OUTPUT);/*SCLK*/
+	if(p_SPI_Config->SPI_Mode == SPI_MODE_MASTER)
+	{
+		MCAL_GPIO_PinInit(GPIOB_ID, GPIO_PIN4, GPIO_PIN_OUTPUT);/*SS*/
+		MCAL_GPIO_PinInit(GPIOB_ID, GPIO_PIN5, GPIO_PIN_OUTPUT);/*MOSI*/
+		MCAL_GPIO_PinInit(GPIOB_ID, GPIO_PIN6, GPIO_PIN_INPUT);/*MISO*/
+		MCAL_GPIO_PinInit(GPIOB_ID, GPIO_PIN7, GPIO_PIN_OUTPUT);/*SCLK*/
+	}else if(p_SPI_Config->SPI_Mode == SPI_MODE_SLAVE)
+	{
+		MCAL_GPIO_PinInit(GPIOB_ID, GPIO_PIN4, GPIO_PIN_INPUT);/*SS*/
+		MCAL_GPIO_PinInit(GPIOB_ID, GPIO_PIN5, GPIO_PIN_INPUT);/*MOSI*/
+		MCAL_GPIO_PinInit(GPIOB_ID, GPIO_PIN6, GPIO_PIN_OUTPUT);/*MISO*/
+		MCAL_GPIO_PinInit(GPIOB_ID, GPIO_PIN7, GPIO_PIN_INPUT);/*SCLK*/
+	}
 
-	/*Master Enable, No Interrupt, LSB Transmitted First, Sample In Raising, SCLK Low Is Idle, Fcpu/4 */
-	SPCR = (1<<SPE) | (1<<MSTR);
+	if(p_SPI_Config->SPI_IRQ == SPI_IRQ_ENABLE)
+	{
+		gp_CallBack = (p_SPI_Config->p_SPI_ISR);
+		SEI();
+	}
+
+	/* Disable Double Speed*/
 	SPSR &= (~(1<<SPI2X));
+
+	/*Configure SPI*/
+	SPCR = (p_SPI_Config->SPI_IRQ) | (p_SPI_Config->SPI_DataOrder) | (p_SPI_Config->SPI_Mode) | (p_SPI_Config->SPI_ClockPolarity) | (p_SPI_Config->SPI_ClockPhase) | (p_SPI_Config->SPI_Prescaler);
 
 }
 
-void MCAL_SPI_InitSlave(void)
+void MCAL_SPI_deinit(void)
 {
-	/*Configure The Pins of SPI*/
-	MCAL_GPIO_PinInit(GPIOB_ID, GPIO_PIN4, GPIO_PIN_INPUT);/*SS*/
-	MCAL_GPIO_PinInit(GPIOB_ID, GPIO_PIN5, GPIO_PIN_INPUT);/*MOSI*/
-	MCAL_GPIO_PinInit(GPIOB_ID, GPIO_PIN6, GPIO_PIN_OUTPUT);/*MISO*/
-	MCAL_GPIO_PinInit(GPIOB_ID, GPIO_PIN7, GPIO_PIN_INPUT);/*SCLK*/
-
-	/* Slave Enable, No Interrupt, LSB Transmitted First, Sample In Raising, SCLK Low Is Idle, Fcpu/4 */
-	SPCR = (1<<SPE);
+	/* Return Registers to Default Value */
+	SPCR = (0x00);
 	SPSR &= (~(1<<SPI2X));
 }
 
-uint8_t MCAL_SPI_SendReceiveByte(uint8_t a_data)
+uint8_t MCAL_SPI_sendReceiveByte(uint8_t a_data,uint8_t a_Polling)
 {
 	/*Assign The Data Register With Data To Transmit*/
 	SPDR = a_data;
 
-	/*Wait Until When a serial transfer is complete, the SPIF Flag is set*/
-	while(BIT_IS_CLEAR(SPSR,SPIF));
+	if(a_Polling == SPI_POLLING_ENABLE)
+	{
+		/*Wait Until When a serial transfer is complete, the SPIF Flag is set*/
+		while(BIT_IS_CLEAR(SPSR,SPIF));
+	}
 
 	/*return The Received Data*/
 	return SPDR;
 }
 
-void MCAL_SPI_SendString(const uint8_t* p_str)
+void MCAL_SPI_sendString(const uint8_t* p_str)
 {
 	uint8_t i = 0;
 	uint8_t dum;
@@ -68,49 +102,49 @@ void MCAL_SPI_SendString(const uint8_t* p_str)
 	while((p_str[i]) != '\0')
 	{
 		/* dum Variable is just for the Dummy Data Received */
-		dum = MCAL_SPI_SendReceiveByte(p_str[i]);
+		dum = MCAL_SPI_sendReceiveByte(p_str[i],SPI_POLLING_ENABLE);
 		i++;
 	}
 }
 
-void MCAL_SPI_ReceiveString(uint8_t* p_str)
+void MCAL_SPI_receiveString(uint8_t* p_str)
 {
 	uint8_t i = 0;
 
 	/*Receive First Byte*/
-	p_str[i] = MCAL_SPI_SendReceiveByte(DEFAULT_DATA);
+	p_str[i] = MCAL_SPI_sendReceiveByte(DEFAULT_DATA,SPI_POLLING_ENABLE);
 
 	/*Receive The Rest of String*/
 	while(p_str[i] != '#')
 	{
 		i++;
-		p_str[i] = MCAL_SPI_SendReceiveByte(DEFAULT_DATA);
+		p_str[i] = MCAL_SPI_sendReceiveByte(DEFAULT_DATA,SPI_POLLING_ENABLE);
 	}
 	p_str[i] = '\0';
 }
 
-void MCAL_SPI_SendNumber(uint32_t a_num)
+void MCAL_SPI_sendNumber(uint32_t a_num)
 {
 	/* Accessing The Number Byte By Byte*/
 	uint8_t *p_temp =(uint8_t*)(&a_num);
 	uint8_t dum;
 
-	dum = MCAL_SPI_SendReceiveByte(p_temp[0]);
-	dum = MCAL_SPI_SendReceiveByte(p_temp[1]);
-	dum = MCAL_SPI_SendReceiveByte(p_temp[2]);
-	dum = MCAL_SPI_SendReceiveByte(p_temp[3]);
+	dum = MCAL_SPI_sendReceiveByte(p_temp[0],SPI_POLLING_ENABLE);
+	dum = MCAL_SPI_sendReceiveByte(p_temp[1],SPI_POLLING_ENABLE);
+	dum = MCAL_SPI_sendReceiveByte(p_temp[2],SPI_POLLING_ENABLE);
+	dum = MCAL_SPI_sendReceiveByte(p_temp[3],SPI_POLLING_ENABLE);
 }
 
-uint32_t MCAL_SPI_ReceiveNumber(void)
+uint32_t MCAL_SPI_receiveNumber(void)
 {
 	uint32_t num;
 	/* Accessing The Number Byte By Byte*/
 	uint8_t *p_temp =(uint8_t*)(&num);
 
-	p_temp[0] = MCAL_SPI_SendReceiveByte(DEFAULT_DATA);
-	p_temp[1] = MCAL_SPI_SendReceiveByte(DEFAULT_DATA);
-	p_temp[2] = MCAL_SPI_SendReceiveByte(DEFAULT_DATA);
-	p_temp[3] = MCAL_SPI_SendReceiveByte(DEFAULT_DATA);
+	p_temp[0] = MCAL_SPI_sendReceiveByte(DEFAULT_DATA,SPI_POLLING_ENABLE);
+	p_temp[1] = MCAL_SPI_sendReceiveByte(DEFAULT_DATA,SPI_POLLING_ENABLE);
+	p_temp[2] = MCAL_SPI_sendReceiveByte(DEFAULT_DATA,SPI_POLLING_ENABLE);
+	p_temp[3] = MCAL_SPI_sendReceiveByte(DEFAULT_DATA,SPI_POLLING_ENABLE);
 
 	return num;
 }
--- Drivers/MCAL/SPI/spi.h
+++ Drivers/MCAL/SPI/spi.h
@@ -17,8 +17,65 @@
  *                                Includes                                       *
  ================================================================================*/
 #include "atmega32.h"
+#include "std_types.h"
+
+/*===============================================================================
+ *                            User Type Definitions                              *
+ ================================================================================*/
+
+typedef struct
+{
+	uint8_t SPI_Mode;			/* Specifies The SPI Mode (Master,Slave) Regarding to @ref SPI Mode*/
+
+	uint8_t SPI_DataOrder;		/* Specifies The SPI DataOrder Regarding to @ref DataOrder */
+
+	uint8_t SPI_ClockPolarity;	/* Specifies The SPI ClockPolarity Regarding to @ref ClockPolarity */
+
+	uint8_t SPI_ClockPhase;		/* Specifies The SPI ClockPhase Regarding to @ref ClockPhase */
+
+	uint8_t SPI_Prescaler;		/* Specifies The SPI Prescaler Regarding to @ref Prescaler */
+
+	uint8_t SPI_IRQ;			/* Specifies The SPI Interrupt Mask Regarding to  @ref SPI Interrupt Enable/Disable */
+
+	void(*p_SPI_ISR)(void);		/* SET The Call Back Function That Will Be Called In ISR */
+
+}SPI_Config_t;
 
 
+/*===============================================================================
+ *                       Macros Configuration References                         *
+ ================================================================================*/
+/*@ref SPI Mode*/
+#define SPI_MODE_MASTER					(0x50U)
+#define SPI_MODE_SLAVE					(0x40U)
+
+/*@ref DataOrder */
+#define SPI_DATA_ORDER_LSB				(0x20U)
+#define SPI_DATA_ORDER_MSB				(0x00U)
+
+/* @ref ClockPolarity */
+#define SPI_CLK_POLARITY_HIGH			(0x08U)/* CLK Idle Is High */
+#define SPI_CLK_POLARITY_LOW			(0x00U)/* CLK Idle Is Low */
+
+/* @ref ClockPhase */
+#define SPI_CLK_PHASE_LEAD				(0x00U)/* Sampling at Leading */
+#define SPI_CLK_PHASE_LAST				(0x04U)/* Sampling at Trailing */
+
+/* @ref Prescaler */
+#define SPI_CLK_PRESCALER_4				(0x00U)
+#define SPI_CLK_PRESCALER_16			(0x01U)
+#define SPI_CLK_PRESCALER_64			(0x02U)
+#define SPI_CLK_PRESCALER_128			(0x03U)
+
+/* @ref SPI Interrupt Enable/Disable */
+#define SPI_IRQ_ENABLE					(0x80U)
+#define SPI_IRQ_DISABLE					(0x00U)
+
+typedef enum
+{
+	SPI_POLLING_DISABLE,
+	SPI_POLLING_ENABLE
+}SPI_Polling_Mecanism;
 /*===============================================================================
  *                     				  Macros 			                         *
  ================================================================================*/
@@ -30,20 +87,20 @@
  ================================================================================*/
 
 /**===============================================================================
- * Function Name  : MCAL_SPI_InitMaster.
- * Brief          : Function To Initialize The SPI as Master.
- * Parameter (in) : None.
+ * Function Name  : MCAL_SPI_Init.
+ * Brief          : Function To Initialize The SPI.
+ * Parameter (in) : Pointer to SPI Configuration.
  * Return         : None.
- * Note           : No Interrupt, LSB Transmitted First, Sample In Raising, SCLK Low Is Idle */
-void MCAL_SPI_InitMaster(void);
+ * Note           : Normal Speed 																		*/
+void MCAL_SPI_init(SPI_Config_t *p_SPI_Config);
 
 /**===============================================================================
- * Function Name  : MCAL_SPI_InitSlave.
- * Brief          : Function To Initialize The SPI as Slave.
+ * Function Name  : MCAL_SPI_Deinit.
+ * Brief          : Function To Uninitialize The SPI.
  * Parameter (in) : None.
  * Return         : None.
- * Note           : No Interrupt, LSB Transmitted First, Sample In Raising, SCLK Low Is Idle */
-void MCAL_SPI_InitSlave(void);
+ * Note           : None.																				 */
+void MCAL_SPI_deinit(void);
 
 /**===============================================================================
  * Function Name  : MCAL_SPI_SendReceiveByte.
@@ -51,7 +108,7 @@ void MCAL_SPI_InitSlave(void);
  * Parameter (in) : Data to Send.
  * Return         : Received Data.
  * Note           : None. */
-uint8_t MCAL_SPI_SendReceiveByte(uint8_t a_data);
+uint8_t MCAL_SPI_sendReceiveByte(uint8_t a_data,uint8_t a_Polling);
 
 /**===============================================================================
  * Function Name  : MCAL_SPI_SendString.
@@ -59,7 +116,7 @@ uint8_t MCAL_SPI_SendReceiveByte(uint8_t a_data);
  * Parameter (in) : The String To Send.
  * Return         : None.
  * Note           : None. */
-void MCAL_SPI_SendString(const uint8_t * p_str);
+void MCAL_SPI_sendString(const uint8_t * p_str);
 
 /**===============================================================================
  * Function Name  : MCAL_SPI_ReceiveString.
@@ -67,7 +124,7 @@ void MCAL_SPI_SendString(const uint8_t * p_str);
  * Parameter (in) : The Buffer Address To Receive String.
  * Return         : None.
  * Note           : None. */
-void MCAL_SPI_ReceiveString(uint8_t * p_str);
+void MCAL_SPI_receiveString(uint8_t * p_str);
 
 /**===============================================================================
  * Function Name  : MCAL_SPI_SendNumber.
@@ -75,7 +132,7 @@ void MCAL_SPI_ReceiveString(uint8_t * p_str);
  * Parameter (in) : The Number To Send.
  * Return         : None.
  * Note           : None. */
-void MCAL_SPI_SendNumber(uint32_t a_num);
+void MCAL_SPI_sendNumber(uint32_t a_num);
 
 /**===============================================================================
  * Function Name  : MCAL_SPI_ReceiveNumber.
@@ -83,6 +140,6 @@ void MCAL_SPI_SendNumber(uint32_t a_num);
  * Parameter (in) : None.
  * Return         : The Received Number.
  * Note           : None. */
-uint32_t MCAL_SPI_ReceiveNumber(void);
+uint32_t MCAL_SPI_receiveNumber(void);
 
 #endif /* INC_SPI_H_ */
