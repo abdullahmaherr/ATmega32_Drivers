997d4f2b5bba70379e1f575cd2c4b1f9a7f105b3
Abdullah Maher <abdullahhegazi10@gmail.com>
2023-11-15 21:50:28 +0200

    Update

--- Drivers/HAL/Sensors/Temperature Sensor/lm35.c
+++ Drivers/HAL/Sensors/Temperature Sensor/lm35.c
@@ -30,7 +30,7 @@
 void HAL_LM35_ADC_init(void)
 {
 	ADC_Config_t LM35_config;
-	LM35_config.ADC_VoltRef = LM35_ADC_Vref_Source;
+	LM35_config.ADC_VoltRef = LM35_ADC_Vref_SOURCE;
 	LM35_config.ADC_Prescaler = ADC_PRESCALE_8;
 	MCAL_ADC_init(&LM35_config);
 }
@@ -42,7 +42,7 @@ uint8_t HAL_LM35_getTemp(void)
 
 	adc_value = MCAL_ADC_readChannel(LM35_CHANNEL);
 
-	temp = (uint8_t)(((uint32_t)adc_value * LM35_MAX_TEMP * LM35_ADC_Vref_Value)/(LM35_ADC_MAX_RES * LM35_MAX_VOLT));
+	temp = (uint8_t)(((uint32_t)adc_value * LM35_MAX_TEMP * LM35_ADC_Vref_VALUE)/(LM35_ADC_MAX_RES * LM35_MAX_VOLT));
 
 	return temp;
 
